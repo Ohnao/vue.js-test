7c9fb927b7110ba0b6ab7d8e171287662ebd589b
naoya.ohsaki <naoya.ohsaki@gaiax.com>
2018-07-24 18:06:53 +0900

    [WIP]vue.jsのエラーが未修正

---
+++ src/vue.js-test/resources/views/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="{{ config('app.locale') }}">
+  <head>
+    <meta cahraset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>vue.js-test</title>
+
+    <link rel="stylesheet" href="css/app.css">
+
+    <script>
+      window.Laravel = {};
+      window.Laravel.csrf_Token = "{{ csrf_token() }}"
+    </script>
+  </head>
+  <body>
+    <div id="app-7">
+            <ol>
+                <todo-item v-for="item in groceryList" v-bind:todo="item"></todo-item>
+            </ol>
+      </div>
+
+        <script src=" {{ mix('js/app.js') }} "></script>
+  </body>
+</html>
